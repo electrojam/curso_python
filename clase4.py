3b9d5e9960d8360220bbab001b51c56a8493ef1b
electrojam <electrojam@hotmail.com>
2024-09-01 03:58:56 +0000

    Manipulación de Cadenas de Texto en Python5 / 35

---
+++ clase4.py
@@ -0,0 +1,23 @@
+name = "Julio"
+print(type(name))
+
+name = '''Julio
+
+
+
+'''
+print(name)
+
+name = 'Julio Alfonso'
+print(name[0])
+print(name[1])
+print(name[-1])
+
+
+last_name = 'Mesa Perez'
+print(name * 5)
+print(name + " " + last_name)
+print(len(last_name))
+
+print(name.lower())
+print(name.upper())
