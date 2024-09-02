1b5fe7cd1cfb22e03948e84122dc6c5dd4c75595
electrojam <electrojam@hotmail.com>
2024-09-02 21:51:11 +0000

    Generadores e Iteradores yield y fibonacci

--- clase18.py
+++ clase18.py
@@ -36,3 +36,28 @@ iter_text = iter(text)
 #Iterar en la cadena
 for char in iter_text:
     print(char)
+
+######## Generador simple
+
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+
+for value in my_generator():
+    print("\n",value)
+
+
+# FIBONACCI
+# 0 1 1 2 3 5 8 13 21
+def fibo(max):
+    a, b= 0, 1
+    # a = 0
+    # b = 1
+    while a < max:
+        yield a
+        a, b = b, a + b
+        # a = b
+        # b = a + b
+for num in fibo(10):
+    print(num)
